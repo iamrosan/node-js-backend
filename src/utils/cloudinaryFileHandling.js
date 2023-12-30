ef646c36af915b81806ab1c4c9dfc61f7254e596
iamrosan <roshanshrestha21.63@gmail.com>
2023-12-30 23:12:06 +0545

    feat: cloudinary configured

---
+++ src/utils/cloudinaryFileHandling.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const cloudinaryUpload = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload file on cloudinary
+    const response = await cloudinary.v2.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file uploaded successfully
+    console.log("File uploaded on cloudinary : ", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //removes the locally saved temporary file as the upload got failed
+  }
+};
+
+export { cloudinaryUpload };
