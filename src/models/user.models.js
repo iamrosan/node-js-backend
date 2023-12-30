348fd9d9920a5fe628c3aa6047238968d7c284ba
iamrosan <roshanshrestha21.63@gmail.com>
2023-12-30 16:33:58 +0545

    feat: model setup

---
+++ src/models/user.models.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // url of third party will be used; cloudnary
+      required: true,
+    },
+    coverImage: {
+      type: String, // url of third party will be used; cloudnary
+    },
+    watchHistory: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", UserSchema);
