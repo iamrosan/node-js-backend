825741975470aa2fa62283c1a84f72a3bd990a3b
iamrosan <roshanshrestha21.63@gmail.com>
2023-12-30 15:51:23 +0545

    feat: express conf setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server Connected to PORT : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB CONNECTION FAILED");
+  });
 
 /*
 import express from "express";
