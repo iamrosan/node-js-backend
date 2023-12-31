3e9838ac5c98b010d40b372ab4ebdcb44ace0901
iamrosan <roshanshrestha21.63@gmail.com>
2024-01-01 00:05:04 +0545

    fix: HOF return fixes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -14,7 +14,9 @@
 // export default asyncHandler;
 
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
+
+export default asyncHandler;
